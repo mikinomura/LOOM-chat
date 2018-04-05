e6cdb3e84dcd88322a39c63f46708970258285b2
Miki Nomura <miki@fyi.tokyo>
2018-04-05 15:41:59 +0900

    Initial Commit

---
+++ LOOM-Chat-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LOOM-Chat-App
+//
+//  Created by Miki Nomura on 4/5/18.
+//  Copyright © 2018 Miki Nomura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
